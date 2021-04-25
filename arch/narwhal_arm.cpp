227c3545553638ae1b0ea5bda963bf65686a9837
Tucker Osman <osmiumusa@gmail.com>
2021-04-25 02:34:59 -0500

    refine mapping UI and prepare for more mapping options

--- arch/narwhal_arm.cpp
+++ arch/narwhal_arm.cpp
@@ -10,7 +10,6 @@
 #define ARM_CODE "\x37\x00\xa0\xe3\x03\x10\x42\xe0" // mov r0, #0x37; sub r1, r2, r3
 #define ADDRESS 0x10000
 
-void show_arm_config_window();
 void show_arm_cpu_window();
 void show_arm_memory_window();
 
@@ -18,18 +17,9 @@ void show_arm_windows() {
     if(ctx.uc == NULL) {
         uc_open(UC_ARCH_ARM, UC_MODE_ARM, &ctx.uc);
     }
-    show_arm_config_window();
     show_arm_cpu_window();
     show_arm_memory_window();
 }
-
-void show_arm_config_window() {
-    ImGui::Begin("ARM Environment", &ui.config_window_open);
-    ImGui::Button("Memory Map");
-    ImGui::End();
-}
-
-void* mem = NULL;
 
 void show_arm_cpu_window() {
     ImGui::Begin("ARM CPU State", &ui.cpu_window_open, ImGuiWindowFlags_AlwaysAutoResize);
@@ -68,7 +58,7 @@ void show_arm_cpu_window() {
     if(ImGui::Button("Step")) {
         uint64_t pc;
         uc_reg_read(ctx.uc, UC_ARM_REG_PC, &pc);
-        printf("Starting emulation from %X to %X\n", pc, pc+4);
+        printf("Starting emulation from %08lX to %08lX\n", pc, pc+4);
         uc_err err = uc_emu_start(ctx.uc, pc, pc+8, 0, 1);
         if (err) {
             printf("Failed on uc_emu_start() with error returned: %u\n", err);
@@ -80,16 +70,52 @@ void show_arm_cpu_window() {
 
 void show_arm_memory_window() {
     static MemoryEditor mem_edit;
+    static char name_buf[64] = "";
+    static char addr_buf[32] = ""; 
+    static char size_buf[32] = ""; 
     
     ImGui::Begin("Memory Regions", &ui.memory_window_open);
     
     if (ImGui::BeginTabBar("MyTabBar", ImGuiTabBarFlags_TabListPopupButton)) {
-        for(struct memory_region region : ctx.mapped_regions) {
+        for(uint64_t i = 0; i < ctx.mapped_regions.size(); i++) {
+            struct memory_region region = ctx.mapped_regions.at(i); 
             if (ImGui::BeginTabItem(region.name)) {
                 ImGui::Text(region.name);
-                ImGui::Text("addr: 0x%X", region.address);
-                ImGui::Text("size: 0x%X", region.size);
-                ImGui::Text("perm: %d", region.perms);
+                ImGui::Text("Address: 0x%X", region.address);
+                ImGui::SameLine();
+                ImGui::Text("Size: 0x%X", region.size);
+                if(region.perms & 1) {
+                    ImGui::Text("Readable");
+                    ImGui::SameLine();
+                }
+                if (region.perms & 2) {
+                    ImGui::Text("Writable");
+                    ImGui::SameLine();
+                }
+                if (region.perms & 4) {
+                    ImGui::Text("Executable");
+                    ImGui::SameLine();
+                }
+                ImGui::Text("");
+
+                if (ImGui::Button("Edit")) {
+                    printf("EDIT %s\n", region.name);
+                }
+                ImGui::SameLine();
+                
+                if (ImGui::Button("Clone")) {
+                    printf("CLONE %s\n", region.name);
+                }
+                ImGui::SameLine();
+
+                ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(1.0f, 0.0f, 0.0f));
+                ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor(1.0f, 0.3f, 0.3f));
+                ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor(0.8f, 0.0f, 0.0f));
+                if(ImGui::Button("Delete")) {
+                    printf("DELETE %s\n", region.name);
+                }
+                ImGui::PopStyleColor(3);
+                
                 mem_edit.DrawContents(region.ptr, region.size, region.address);
                 ImGui::EndTabItem();
             }
@@ -107,31 +133,38 @@ void show_arm_memory_window() {
             ImGui::Text("Define a new region of memory usable by the emulation.");
             ImGui::Separator();
 
-            static char buf1[64] = ""; ImGui::InputText("Name", buf1, 64);
-            static char buf2[64] = ""; ImGui::InputText("Starting Address", buf2, 64, ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_CharsUppercase);
-            static char buf3[64] = ""; ImGui::InputText("Size", buf3, 64, ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_CharsUppercase);
+            ImGui::InputText("Name", name_buf, 64);
+            ImGui::InputText("Address (hex, x4KB)", addr_buf, 32, ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_CharsUppercase);
+            ImGui::InputText("Size (hex, x4KB)", size_buf, 32, ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_CharsUppercase);
+
+            long addr = strtol(addr_buf, NULL, 16);
+            long size = strtol(size_buf, NULL, 16);
+            bool valid = (addr > 0) && (size > 0) && (addr % 4096 == 0) && (size % 4096 == 0);
 
             static bool readable, writable, executable;
             ImGui::Checkbox("Readable", &readable); ImGui::SameLine();
             ImGui::Checkbox("Writable", &writable); ImGui::SameLine();
             ImGui::Checkbox("Executable", &executable);
-
-            if (ImGui::Button("OK", ImVec2(120, 0))) {
-                struct memory_region new_memory_region;
-                new_memory_region.name = (char*) malloc(64);
-                memcpy(new_memory_region.name, buf1, 64);
-                new_memory_region.address = strtol(buf2, NULL, 16);
-                new_memory_region.size = strtol(buf3, NULL, 16);
-                new_memory_region.perms = readable ? UC_PROT_READ : 0;
-                new_memory_region.perms |= writable ? UC_PROT_WRITE : 0;
-                new_memory_region.perms |= executable ? UC_PROT_EXEC: 0;
-                new_memory_region.ptr = calloc(1, new_memory_region.size);
-                ctx.mapped_regions.push_back(new_memory_region);
-                uc_err err = uc_mem_map_ptr(ctx.uc, new_memory_region.address, new_memory_region.size, new_memory_region.perms, new_memory_region.ptr);
-                if (err) {
-                    printf("Failed on uc_mem_map_ptr() with error returned: %u with %08X, %08X, %08X, %08X, %08X\n", err, ctx.uc, new_memory_region.address, new_memory_region.size, new_memory_region.perms, new_memory_region.ptr);
+            if (valid) {
+                if (ImGui::Button("OK", ImVec2(120, 0))) {
+                    struct memory_region new_memory_region;
+                    new_memory_region.name = (char*) malloc(64);
+                    memcpy(new_memory_region.name, name_buf, 64);
+                    new_memory_region.address = addr;
+                    new_memory_region.size = size;
+                    new_memory_region.perms = readable ? UC_PROT_READ : 0;
+                    new_memory_region.perms |= writable ? UC_PROT_WRITE : 0;
+                    new_memory_region.perms |= executable ? UC_PROT_EXEC: 0;
+                    new_memory_region.ptr = calloc(1, new_memory_region.size);
+                    ctx.mapped_regions.push_back(new_memory_region);
+                    uc_err err = uc_mem_map_ptr(ctx.uc, new_memory_region.address, new_memory_region.size, new_memory_region.perms, new_memory_region.ptr);
+                    if (err) {
+                        printf("Failed on uc_mem_map_ptr() with error returned: %u with %p, %08lX, %08lX, %08X, %p\n", err, ctx.uc, new_memory_region.address, new_memory_region.size, new_memory_region.perms, new_memory_region.ptr);
+                    }
+                    ImGui::CloseCurrentPopup();
                 }
-                ImGui::CloseCurrentPopup();
+            } else {
+                ImGui::Text("Must be 4KB aligned");
             }
             ImGui::SetItemDefaultFocus();
             ImGui::SameLine();
