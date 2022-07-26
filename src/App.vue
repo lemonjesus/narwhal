83a019fc1c4f78defe708596e9e983cb53904f8e
Tucker Osman <osmiumusa@gmail.com>
2022-07-26 12:20:08 -0500

    add bootstrap, titlebar, and unicorn

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <golden-layout class="hscreen">
-    <gl-row>
-      <gl-component title="CPU State">
-        <ProcessorWindow msg="hello"></ProcessorWindow>
-      </gl-component>
-      <gl-stack>
-        <gl-component title="Memory">
-          <MemoryWindow msg="memory"></MemoryWindow>
+  <div>
+    <div id="toolbar">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand>Narwhal</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item-dropdown text="File">
+              <b-dropdown-item href="#">New</b-dropdown-item>
+              <b-dropdown-item href="#">Open</b-dropdown-item>
+              <b-dropdown-item href="#">Save</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+    <golden-layout class="hscreen">
+      <gl-row>
+        <gl-component title="CPU State">
+          <ProcessorWindow msg="hello"></ProcessorWindow>
         </gl-component>
-        <gl-component title="component3">
-          <h1>Component 3</h1>
-        </gl-component>
-      </gl-stack>
-    </gl-row>
-  </golden-layout>
+        <gl-stack>
+          <gl-component title="Memory">
+            <MemoryWindow msg="memory"></MemoryWindow>
+          </gl-component>
+          <gl-component title="component3">
+            <h1>Component 3</h1>
+          </gl-component>
+        </gl-stack>
+      </gl-row>
+    </golden-layout>
+  </div>
+
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,16 @@ import 'golden-layout/src/css/goldenlayout-dark-theme.css'
 
 Vue.config.productionTip = false
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import './app.scss'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
